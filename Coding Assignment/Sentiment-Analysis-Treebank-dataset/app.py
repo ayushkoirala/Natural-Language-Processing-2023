b74e362968f6b8a6a1e2b04660a24751e1cf8b29
ayush koirala <50586491+ayushkoirala@users.noreply.github.com>
2023-02-17 19:21:28 +0700

    update
    update

--- Coding Assignment/Sentiment-Analysis-Treebank-dataset/app.py
+++ Coding Assignment/Sentiment-Analysis-Treebank-dataset/app.py
@@ -8,9 +8,24 @@ from utils.backbone import topwords
 import asyncio
 import nest_asyncio
 nest_asyncio.apply()
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 class ResponseModel(BaseModel):
     text: str
